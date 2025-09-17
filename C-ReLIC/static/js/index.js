2006c33814d24091d48799ef047276e1464664f4
eedavidwu <43716059+eedavidwu@users.noreply.github.com>
2025-09-17 13:35:26 +0100

    Add files via upload

---
+++ C-ReLIC/static/js/index.js
@@ -0,0 +1,136 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+// var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
+// var NUM_INTERP_FRAMES = 240;
+
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
+
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
+
+
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    loop: true,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  }
+
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+
+  // Loop on each carousel initialized
+  for (var i = 0; i < carousels.length; i++) {
+    // Add listener to  event
+    carousels[i].on('before:show', state => {
+      console.log(state);
+    });
+  }
+
+  // Access to bulmaCarousel instance of an element
+  var element = document.getElementById('container-carousel 1');
+
+  if (element && element.bulmaCarousel) {
+    // bulmaCarousel instance is available as element.bulmaCarousel
+    element.bulmaCarousel.on('before-show', function (state) {
+      console.log(state);
+    });
+  }
+
+
+  bulmaSlider.attach();
+
+
+})
+$(document).ready(function () {
+  console.log('document', document)
+
+
+  const acceptButton = document.getElementById('accept-button');
+
+  revealDatalink = document.getElementById('reveal-datalink')
+  datalink = document.getElementById('datalink')
+  datalink.href = "https://www.robots.ox.ac.uk/~nikita/storage/links.json";
+  datalink.textContent = "Download data links";
+  datalink.download = "links.json"
+  datalink.target = "_blank"
+
+
+  function showDataLink() {
+    acceptButton.style.display = "none";
+    revealDatalink.style.display = "block";
+    // acceptButton.parentNode.replaceChild(divlink, acceptButton);
+  }
+
+  acceptButton.addEventListener('click', showDataLink);
+  // acceptButton.addEventListener('click', acceptCookies);
+  // rejectButton.addEventListener('click', rejectCookies);
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
+
